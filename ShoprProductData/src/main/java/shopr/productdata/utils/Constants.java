81eb59aa84a59828b2b562cc31e30333448e094a
Neil Allison <neiljallison@gmail.com>
2016-09-28 00:07:52 -0400

    Adding new constants.

--- ShoprProductData/src/main/java/shopr/productdata/utils/Constants.java
+++ ShoprProductData/src/main/java/shopr/productdata/utils/Constants.java
@@ -5,6 +5,9 @@ package shopr.productdata.utils;
  */
 public class Constants {
     public static final String BESTBUY_BULK_PRODUCT_API_BASE = "https://api.bestbuy.com/v1/products.json.zip?apiKey=";
+    public static final String WALMART_TAXONOMY_API_BASE = "http://api.walmartlabs.com/v1/taxonomy?apiKey=";
+    public static final String WALMART_SEARCH_API_BASE = "http://api.walmartlabs.com/v1/search?apiKey=";
 
+    public static final String SHOPR_S3_DATA_BUCKET = "shopr-data.com";
     public static final String SHOPR_PROPERTIES_FILE = "shopr-development.properties";
 }
